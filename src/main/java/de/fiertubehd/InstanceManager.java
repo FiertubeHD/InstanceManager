9755c53bcdcd50e5c6659bd22979c17524a8c934
FiertubeHD <fiertube04383@gmail.com>
2024-09-12 20:02:09 +0200

    Added Asynchronous Support

--- src/main/java/de/fiertubehd/InstanceManager.java
+++ src/main/java/de/fiertubehd/InstanceManager.java
@@ -1,19 +1,19 @@
 package de.fiertubehd;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import de.fiertubehd.fluffyannotationslibrary.annotations.NotNull;
+import de.fiertubehd.fluffyannotationslibrary.annotations.Nullable;
 
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 public class InstanceManager<K, I>
 {
 
-    private static final Map<Class<?>, Class<?>> primitiveWrapperMap;
+    private static final HashMap<Class<?>, Class<?>> primitiveWrapperMap;
 
     static
     {
@@ -30,265 +30,206 @@ public class InstanceManager<K, I>
         primitiveWrapperMap.put(Void.TYPE, Void.TYPE);
     }
 
-    private final Map<K, Map<Integer, I>> instanceMap;
+    private final ConcurrentHashMap<K, ConcurrentHashMap<Integer, I>> instances;
     private final Class<I> instanceClazz;
-
-
-    private final Class<? extends Map> mapClazz;
-    private final Object[] mapArgs;
 
     public InstanceManager(@NotNull Class<I> instanceClazz)
     {
-        this(instanceClazz, HashMap.class, new Object[]{});
-    }
+        if(instanceClazz == null)
+            throw new IllegalArgumentException("InstanceClazz cannot be null.");
 
-    public InstanceManager(@NotNull Class<I> instanceClazz, @NotNull Class<? extends Map> mapClazz, @NotNull Object[] mapArgs) throws RuntimeException
-    {
+        instances = new ConcurrentHashMap<>();
+
         this.instanceClazz = instanceClazz;
-
-        this.mapClazz = mapClazz;
-        this.mapArgs = mapArgs;
-
-        try
-        {
-            instanceMap = createInstance(mapClazz, mapArgs);
-
-        }catch(Exception e)
-        {
-            throw new RuntimeException(e);
-        }
     }
 
 
 
     @Nullable
-    public I getExistingInstance(@NotNull K key, int instanceId)
+    public I getExistingInstance(@Nullable K key, int instanceId)
     {
-        return getInstanceMapEntry(key, instanceId);
+        if(key == null || !isValidInstanceId(instanceId))
+            return null;
+
+        ConcurrentHashMap<Integer, I> innerMap = instances.get(key);
+        if(innerMap == null)
+            return null;
+
+        return innerMap.get(instanceId);
     }
 
-
-
-    @NotNull
-    public I getInstance(@NotNull K key, int instanceId, @NotNull Object[] constructorArguments) throws RuntimeException
+    public I getInstance(@NotNull K key, int instanceId, @NotNull Object[] parameters) throws RuntimeException
     {
-        validateInstanceId(instanceId);
+        if(key == null)
+            throw new IllegalArgumentException("Key cannot be null.");
 
-        if(existsInstanceMapEntry(key, instanceId))
-            return getInstanceMapEntry(key, instanceId);
+        if(!isValidInstanceId(instanceId))
+            throw new IllegalArgumentException("InstanceId cannot be smaller than 0.");
 
-        I instance = createInstance(instanceClazz, constructorArguments);
+        if(parameters == null)
+            throw new IllegalArgumentException("Parameters cannot be null.");
 
-        setInstanceMapEntry(key, instanceId, instance);
+        ConcurrentHashMap<Integer, I> innerMap = instances.computeIfAbsent(key, k -> new ConcurrentHashMap<>());
 
-        return instance;
-    }
-
-    @NotNull
-    public I getInstance(@NotNull K key, int instanceId, @NotNull Constructor<I> constructor, @NotNull Object[] constructorArguments) throws RuntimeException
-    {
-        validateInstanceId(instanceId);
-
-        if(existsInstanceMapEntry(key, instanceId))
-            return getInstanceMapEntry(key, instanceId);
-
-        I instance = createInstance(constructor, constructorArguments);
-
-        setInstanceMapEntry(key, instanceId, instance);
-
-        return instance;
-    }
-
-    public boolean removeInstance(@NotNull K key, int instanceId)
-    {
-        return removeInstanceMapEntry(key, instanceId);
-    }
-
-    public boolean existsInstance(@NotNull K key, int instanceId)
-    {
-        return existsInstanceMapEntry(key, instanceId);
-    }
-
-
-
-    public boolean isInstanceIdAssigned(@NotNull K key, int instanceId)
-    {
-        return instanceMap.containsKey(key) && instanceMap.get(key).containsKey(instanceId);
-    }
-
-    public int getUnassignedInstanceId(@NotNull K key)
-    {
-        if(!instanceMap.containsKey(key))
-            return 0;
-
-        int next = 0;
-
-        Set<Integer> keys = instanceMap.get(key).keySet();
-        while(keys.contains(next))
-            next++;
-
-        return next;
-    }
-
-
-
-    protected synchronized void setInstanceMapEntry(@NotNull K key, int instanceId, @NotNull I instance) throws RuntimeException
-    {
-        if(!instanceMap.containsKey(key))
+        I instance = innerMap.computeIfAbsent(instanceId, integer ->
         {
             try
             {
-                instanceMap.put(key, createInstance(mapClazz, mapArgs));
+                return createInstance(instanceClazz, parameters);
 
             }catch(Exception e)
             {
                 throw new RuntimeException(e);
             }
-        }
+        });
 
-        instanceMap.get(key).put(instanceId, instance);
+        return instance;
     }
 
-    protected boolean removeInstanceMapEntry(@NotNull K key, int instanceId)
+    public boolean unregisterInstance(@NotNull K key, int instanceId)
     {
-        if(!instanceMap.containsKey(key))
+        if(key == null)
+            throw new IllegalArgumentException("Key cannot be null.");
+
+        if(!isValidInstanceId(instanceId))
+            throw new IllegalArgumentException("InstanceId cannot be smaller than 0.");
+
+        ConcurrentHashMap<Integer, I> innerMap = instances.get(key);
+        if(innerMap == null)
             return false;
 
-        return instanceMap.get(key).remove(instanceId) != null;
+        AtomicBoolean result = new AtomicBoolean(false);
+        innerMap.computeIfPresent(instanceId, (integer, i) ->
+        {
+            result.set(true);
+            return null;
+        });
+
+        return result.get();
     }
 
-    @Nullable
-    protected I getInstanceMapEntry(@NotNull K key, int instanceId)
+    public boolean existsInstance(@Nullable K key, int instanceId)
     {
-        return (instanceMap.containsKey(key)) ? instanceMap.get(key).get(instanceId) : null;
+        if(key == null || !isValidInstanceId(instanceId))
+            return false;
+
+        ConcurrentHashMap<Integer, I> innerMap = instances.get(key);
+        if(innerMap == null)
+            return false;
+
+        return innerMap.containsKey(instanceId);
     }
 
-    protected boolean existsInstanceMapEntry(@NotNull K key, int instanceId)
+
+
+    @NotNull
+    public static <E> E createInstance(@NotNull Class<E> instanceClazz, @NotNull Object[] parameters) throws RuntimeException
     {
-        return instanceMap.containsKey(key) && instanceMap.get(key).containsKey(instanceId);
-    }
+        if(instanceClazz == null)
+            throw new IllegalArgumentException("InstanceClazz cannot be null.");
 
+        if(parameters == null)
+            throw new IllegalArgumentException("Parameters cannot be null.");
 
-
-
-
-    @Nullable
-    public static <E> E createInstance(@NotNull Constructor<E> instanceConstructor, @NotNull Object[] constructorArguments)
-    {
-        E instance = null;
         try
         {
-            instanceConstructor.setAccessible(true);
-            instance = instanceConstructor.newInstance(constructorArguments);
-
-        }catch(Exception e)
-        {
-            throw new RuntimeException(e);
-        }
-
-        return instance;
-    }
-
-    @Nullable
-    public static <E> E createInstance(@NotNull Class<E> instanceClazz, @NotNull Object[] constructorArguments) throws RuntimeException
-    {
-        E instance = null;
-        try
-        {
-            Constructor<E> constructor = findMatchingConstructor(instanceClazz, constructorArguments);
+            Constructor<E> constructor = findMatchingConstructor(instanceClazz, parameters);
             constructor.setAccessible(true);
 
-            instance = constructor.newInstance(constructorArguments);
+            return constructor.newInstance(parameters);
+
+        }catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    public static <E> E createInstance(@NotNull Constructor<E> constructor,  @NotNull Object[] parameters) throws RuntimeException
+    {
+        if(constructor == null)
+            throw new IllegalArgumentException("Constructor cannot be null.");
+
+        if(parameters == null)
+            throw new IllegalArgumentException("Parameters cannot be null.");
+
+        try
+        {
+            constructor.setAccessible(true);
+            return constructor.newInstance(parameters);
+
+        }catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    public static <E> Constructor<E> findMatchingConstructor(@NotNull Class<E> instanceClazz, @NotNull Object[] parameters) throws RuntimeException
+    {
+        if(instanceClazz == null)
+            throw new IllegalArgumentException("InstanceClazz cannot be null.");
+
+        if(parameters == null)
+            throw new IllegalArgumentException("Parameters cannot be null.");
+
+        Constructor<?>[] declaredConstructors = null;
+        try
+        {
+            declaredConstructors = instanceClazz.getDeclaredConstructors();
 
         }catch(Exception e)
         {
             throw new RuntimeException(e);
         }
 
-        return instance;
-    }
-
-    @Nullable
-    public static <E> Constructor<E> findMatchingConstructor(@NotNull Class<E> instanceClazz, @NotNull Object[] constructorArguments) throws RuntimeException
-    {
-        for(Constructor<?> declaredConstructor : instanceClazz.getDeclaredConstructors())
+        for(Constructor<?> declaredConstructor : declaredConstructors)
         {
             Class<?>[] declaredParameterTypes = declaredConstructor.getParameterTypes();
-            if(declaredParameterTypes.length != constructorArguments.length) continue;
+            if(declaredParameterTypes.length != parameters.length)
+                continue;
 
             boolean match = true;
             for(int i = 0; i < declaredParameterTypes.length; i++)
             {
-                if(!primitiveToWrapper(declaredParameterTypes[i]).isInstance(constructorArguments[i]))
+                if(!primitiveToWrapper(declaredParameterTypes[i]).isInstance(parameters[i]))
                 {
                     match = false;
                     break;
                 }
             }
 
-            if(match) return (Constructor<E>) declaredConstructor;
+            if(match)
+                return (Constructor<E>) declaredConstructor;
         }
 
-        throw new IllegalArgumentException(
-                "No suitable constructor could be found in the class "
+        throw new IllegalArgumentException
+                        (
+                        "No suitable constructor could be found in the class "
                         + instanceClazz.getName()
-                        + " with the specified arguments("
-                        + Arrays.stream(constructorArguments)
+                        + " with the specified arguments"
+                        + Arrays.stream(parameters)
                         .map(o -> o == null ? null : o.getClass())
                         .map(c -> c == null ? "null" : c.getName())
                         .collect(Collectors.joining(",", "(", ")"))
-        );
+                        );
     }
 
 
+
     @Nullable("if clazz is null")
-    public static Class<?> primitiveToWrapper(final Class<?> clazz)
+    public static Class<?> primitiveToWrapper(@Nullable final Class<?> clazz)
     {
         Class<?> convertedClass = clazz;
         if(clazz != null && clazz.isPrimitive())
-        {
             convertedClass = primitiveWrapperMap.get(clazz);
-        }
 
         return convertedClass;
     }
 
-
-
-    @NotNull
-    public Class<I> getInstanceClazz()
-    {
-        return instanceClazz;
-    }
-
-    @NotNull
-    public Class<? extends Map> getMapClazz()
-    {
-        return mapClazz;
-    }
-
-    @NotNull
-    public Object[] getMapArgs()
-    {
-        return mapArgs;
-    }
-
-    @NotNull
-    public Map<K, Map<Integer, I>> getInstanceMap()
-    {
-        return instanceMap;
-    }
-
-
-
     public static boolean isValidInstanceId(int instanceId)
     {
-        return instanceId >= 0 && instanceId <= 999;
+        return instanceId >= 0;
     }
 
-    public static void validateInstanceId(int instanceId) throws RuntimeException
-    {
-        if(!isValidInstanceId(instanceId))
-            throw new IllegalArgumentException("InstanceId out of range. Must be 0-999");
-    }
 }
